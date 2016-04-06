fe89787d6392a50b42d2ec4c7757eba715f451bd
OllieBuilds <scarborough.oliver@gmail.com>
2016-04-06 10:08:02 -0400

    fixes and organized winCases logic
    seems to work

--- assets/scripts/gameplay.js
+++ assets/scripts/gameplay.js
@@ -16,44 +16,51 @@ const announceWinner = function(a) {
 };
 
 const winCases = function winCases(array) {
-  if(array[0] !== ""){
-    // left column wins
-    if(array[3] !== "" && array[3] === array[6]){
-      return announceWinner(array[0]);
-      }
-    // diagonal wins
-    else if (array[4] !== "" && array[4] === array[8]){
-      return announceWinner(array[0]);
-    }
-    // top horizontal
-    else if (array[1] !== "" && array[1] === array[2]){
-      return announceWinner(array[0]);
-    }
+  // global --> top row 0
+if (array[0] !== ""){
+  // top row: horizontal
+  if(array[1] !== "" && array[1] === array[2]){
+    return announceWinner(array[0]);
   }
-
-  else if(array[1] !== ""){
-    // center column wins
-    if(array[4] !== "" && array[4] === array[7]){
-      return announceWinner(array[1]);
-      }
-    }
-
-  else if(array[2] !== ""){
-    // right column wins
-    if(array[5] !== "" && array[5] === array[8]){
-      return announceWinner(array[2]);
-    // diagonal wins
-    }else if (array[4] !== "" && array[4] === array[6]){
-      return announceWinner(array[2]);
-    }
+  // top row: vertical
+  else if(array[3] !== "" && array[3] === array[6]){
+    return announceWinner(array[0]);
   }
-
-  else if(array[3] !== ""){
-    // center horizontal
-    if(array[4] !== "" && array[4] === array[5]){
-      return announceWinner(array[3]);
-    }
+  // top left diagonal
+  else if(array[4] !== "" && array[4] === array[8]){
+    return announceWinner(array[0]);
   }
+  //global --> 1
+}else if(array[1] !== ""){
+  // center column vertical
+  if(array[4] !== "" && array[4] === array[7]){
+    return announceWinner(array[1]);
+  }
+  // global --> 2
+}else if(array[2] !== ""){
+  // right column vertical
+  if(array[5] !== "" && array[5] === array[8]){
+    return announceWinner(array[2]);
+  }
+  // right corner diagonal
+  if(array[4] !== "" && array[4] === array[6]){
+    return announceWinner(array[2]);
+  }
+  // global --> 3
+}else if(array[3] !== ""){
+  // middle row horizontal
+  if(array[4] !== "" && array[4] === array[5]){
+    return announceWinner(array[3]);
+  }
+  // global --> 6
+}else if(array[6] !== ""){
+  // bottom row horizontal
+  if(array[7] !== "" && array[7] === array[8]){
+    return announceWinner(array[6]);
+  }
+  // global
+}
+  // end function
 };
 
 $('td').click( function () {

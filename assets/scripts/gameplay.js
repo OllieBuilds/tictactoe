a1c30a6e6e43e5f00cbc65e1e02ecc0d6b0c1424
OllieBuilds <scarborough.oliver@gmail.com>
2016-04-05 21:36:29 -0400

    gameArray is now updating with td id's
    this is good news!

--- assets/scripts/gameplay.js
+++ assets/scripts/gameplay.js
@@ -1,30 +1,42 @@
 'use strict';
 
 
-let gameArray = [
-];
 
+
+let gameArray = [
+  "", "", "","","","","","",""
+];
 
 let numTurn = 0;
 
 
 
+const winCases = function winCases(array) {
+  for(let i = 0; i < array.length; i++) {
+    if (array[i] === array[i+3] === array[1+6]){
+      console.log('winner!');
+    }
+  }
+};
 
 
 $('td').click( function () {
   if (numTurn % 2 === 0 && $(this).text() === '') {
       $(this).removeClass('playertwo').addClass('playerone');
       $(this).text('X');
-      gameArray.push('X');
+      gameArray[numTurn] = $(this).attr('id');
       numTurn += 1;
       console.log(gameArray);
       // console.log ($(this).get());
     } else if (numTurn % 2 !== 0 && $(this).text() === ''){
       $(this).removeClass('playerone').addClass('playertwo');
       $(this).text('O');
-      gameArray.push('O');
+      gameArray[numTurn] = 'O';
       numTurn += 1;
       console.log(gameArray);
+    }
+    for(let i = 0; i < gameArray.length; i++) {
+      winCases(gameArray);
     }
   });
 
